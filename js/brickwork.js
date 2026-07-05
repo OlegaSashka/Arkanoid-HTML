3f112b1cdc58679ee6b8810ad285838361e2c9f7
OlegSashKa <83597726+OlegSashKa@users.noreply.github.com>
2026-07-05 15:37:32 +0300

    feat: implement brickwork grid factory for pattern layouts

---
+++ js/brickwork.js
@@ -0,0 +1,36 @@
+import { Brick } from "./brick.js";
+
+export class BrickWork {
+    constructor(startX, startY, totalWidth, totalHeight, cols = 10, rows = 4){
+        const generatedBricks = [];
+
+        const brickWidth = totalWidth / cols;
+        const brickHeight = totalHeight / rows;
+        let color = null;
+        for (let row = 0; row < rows; row++) {
+            const y = startY + row * brickHeight;
+
+            let brick = new Brick();
+            color = this._getRandomColor();
+            for (let col = 0; col < cols; col++) {
+                const x = startX + col * brickWidth;
+
+                const brick = new Brick(x,y,brickWidth - 2, brickHeight - 2, 1, color, false);
+
+                generatedBricks.push(brick);
+            }
+        }
+
+        return generatedBricks;
+    }
+
+    _getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+}
