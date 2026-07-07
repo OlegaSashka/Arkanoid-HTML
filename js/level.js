19e67f96a2a2952d9b5bf2d9103cb497a1c3f7ec
OlegSashKa <83597726+OlegSashKa@users.noreply.github.com>
2026-07-07 21:53:47 +0300

    feat: randomize paddle position and brick count on level restart

--- js/level.js
+++ js/level.js
@@ -15,7 +15,7 @@ export class Level extends BaseLevel {
         const startingBall = new Ball(400, 400, 10, 0, 0, 0);
         this.balls[0] = startingBall;
 
-        this.paddle = new Paddle(220, 500, 200, 15, 0, 0, 5, startingBall);
+        this.paddle = new Paddle(this.getRandomInt(0, this.worldWidth - 170), 500, 170, 15, 0, 0, 5, startingBall, this.getRandomInt(30,140));
 
         this.walls = [
             new Wall(0, 0, 20, 600, 0, 0,'vertical'),     // Левая вертикальная стена
@@ -23,6 +23,12 @@ export class Level extends BaseLevel {
             new Wall(0, 0, 800, 10, 0, 0, 'horizontal')    // Верхняя горизонтальная стена (крыша)
         ];
 
-        this.bricks = new BrickWork(20, 50, this.worldWidth - 38, 140, 3, 1);
+        this.bricks = new BrickWork(20, 50, this.worldWidth - 38, 140, this.getRandomInt(5,20), this.getRandomInt(5,20));
+    }
+
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
