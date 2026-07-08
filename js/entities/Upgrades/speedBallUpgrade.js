ad9f060263d6a687e4abf2cd9e358dc808c915a4
OlegSashKa <83597726+OlegSashKa@users.noreply.github.com>
2026-07-08 19:12:10 +0300

    feat: обновлен класс SpeedBallUpgrade для поддержки пользовательских размеров и улучшения логики ускорения шаров

--- js/entities/Upgrades/speedBallUpgrade.js
+++ js/entities/Upgrades/speedBallUpgrade.js
@@ -1,12 +1,20 @@
 import { UpgradeBrick } from './UpgradeBrick.js';
 
-export class SpeedBallUpgrade extends GameObject {
-    constructor(x,y){super(x,y,15,15,3,"#FF5722");}
+
+export class SpeedBallUpgrade extends UpgradeBrick {
+    constructor(x,y, width, height, speed = 3){super(x,y,width,height,speed,"#FF5722");}
 
     applyEffect(entity, level){
         if(level.balls.length > 0){
             level.balls.forEach(ball => {
-                ball.speed += 2;
+                const curBallSpeed = ball.speed;
+                level.resetAllSetings();
+                ball.speed = curBallSpeed + 2.5;
+                console.log("ball.baseSettingsCache.speed " + ball.baseSettingsCache.speed);
+                if(ball.speed >= ball.baseSettingsCache.speed * 4){
+                    ball.speed = ball.baseSettingsCache.speed * 4;
+                }
+                    
             });
             console.log("Все шары ускорены!"); 
         }
