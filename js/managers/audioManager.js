58645bbfbe6826b2a3824b9203e1237fa5b67121
OlegSashKa <83597726+OlegSashKa@users.noreply.github.com>
2026-07-08 16:19:44 +0300

    feat: upgrade and implement Singleton AudioManager infrastructure backed by Howler.js

---
+++ js/managers/audioManager.js
@@ -0,0 +1,73 @@
+
+import { AudioManifest } from "../../assets/audioManifest.js";
+
+export class AudioManager {
+    static #instance = null;
+
+    constructor(baseVolume = 0.1){
+        if(AudioManager.#instance){
+            return AudioManager.#instance;
+        }
+
+        this.sounds = {};
+        this.baseVolume = baseVolume;
+
+        AudioManager.#instance = this;
+    }
+    
+    #asset(relativePath) { 
+        return new URL(`../../assets/${relativePath}`, import.meta.url).href;
+    }
+
+    loadManifest(){
+        try{
+            Object.values(AudioManifest).forEach(sound => {
+                if(sound.key && sound.url){
+                    const resolvedUrl = this.#asset(sound.url);
+                    
+                    this.addAudio(sound.key, resolvedUrl, sound.loop ?? false, sound.volume ?? null);
+                }
+            });
+            console.log(`[AudioManager]: Успешно загружено звуков: ${Object.keys(AudioManifest).length}`);
+        } catch (error) {
+            console.error("[AudioManager]: Ошибка при разборе аудио-манифеста:", error);
+        }
+    }
+
+    addAudio(key, url, loop = false, volume = null){
+        this.sounds[key] = new Howl({
+            src:[url],
+            loop: loop,
+            volume: volume ?? this.baseVolume
+        });
+    }
+    
+    playSound(key, volumeSound = this.baseVolume){
+        if(this.sounds[key]){
+            this.changeVolume(key, volumeSound)
+            this.sounds[key].play();
+        } else {
+            console.warn(`[AudioManager]: Попытка воспроизвести незагруженный звук по ключу: "${key}"`);
+        }
+    }
+
+    playSoundOnce(key, volumeSound = this.baseVolume){
+        this.stopSound(key);
+        this.playSound(key, volumeSound);
+    }
+
+    stopSound(key){
+        if (this.sounds[key]) this.sounds[key].stop();
+    }
+
+    changeVolume(key, volume){
+        if (!this.sounds[key]) return;
+
+        if(volume <= 0) volume = 0;
+        if(volume >= 1) volume = 1;
+
+        this.sounds[key].volume(volume);
+    }
+}
+
+export const audioManager = new AudioManager();
