92304935495d5e8d944a609e0402b2c9faa02558
OlegSashKa <83597726+OlegSashKa@users.noreply.github.com>
2026-07-08 16:21:49 +0300

    feat: добавление новых звуковых эффектов и манифеста аудио

---
+++ assets/audioManifest.js
@@ -0,0 +1,7 @@
+export const AudioManifest = Object.freeze({
+    BOUNCE_PADDLE: {key: 'paddle_hit', url: 'audio/sfx-3.mp3', loop: false, volume: 0.2},
+    BOUNCE_WALL: {key: 'wall_hit', url: 'audio/sfx-4.mp3', loop: false, volume: 0.2},
+    BRICK_HIT: {key: 'brick_hit', url: 'audio/sfx-5.mp3', loop: false, volume: 0.2}
+});
+
+
