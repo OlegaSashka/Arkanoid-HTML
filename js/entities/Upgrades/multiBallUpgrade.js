5cd8c59c4db4835bdbdbe945307dc7518124bfd6
OlegSashKa <83597726+OlegSashKa@users.noreply.github.com>
2026-07-08 19:12:02 +0300

    feat: обновлен конструктор MultiBallUpgrade для поддержки пользовательских размеров и скорости

--- js/entities/Upgrades/multiBallUpgrade.js
+++ js/entities/Upgrades/multiBallUpgrade.js
@@ -2,18 +2,20 @@ import { UpgradeBrick } from './upgradeBrick.js';
 import { Ball } from '../ball.js';
 
 export class MultiBallUpgrade extends UpgradeBrick {
-    constructor(x,y){super(x,y,15,15,3,"#bbff00ff");}
+    constructor(x,y, width, height, speed = 3){super(x,y,width,height,speed,"#bbff00ff");}
 
     applyEffect(entity, level){
         if(level.balls.length > 0){
+            level.resetAllSetings();
+
             const currentBall = level.balls[0];
 
             const newBall = new Ball(
                 currentBall.x,
                 currentBall.y,
                 currentBall.radius,
-                -currentBall.direction.x,
-                currentBall.direction.y,
+                Math.random(),
+                Math.random(),
                 currentBall.speed
             );
 
